4d609c900d6f84fc8ac9032166e6905a709a1f0e
Pablo Morales <pablofmorales@gmail.com>
2018-02-12 17:30:00 +0800

    Transformer inception allowed

---
+++ src/TransformerJoint.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace APITransformer;
+
+use League\Fractal\Serializer\ArraySerializer;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Item as FractalItem;
+
+
+/**
+ * Trait TransformerJoint
+ *
+ * When you need to include transformer inside of your transformer
+ *
+ * @package APITransformer
+ */
+trait TransformerJoint
+{
+
+    /**
+     * @param $model
+     * @param $transformer
+     * @return array
+     */
+    public static function transform($model, $transformer)
+    {
+        $manager = new Manager();
+        $manager->setSerializer(new ArraySerializer());
+        $resource = new FractalItem($model, $transformer);
+        return $manager->createData($resource)->toArray();
+    }
+}
